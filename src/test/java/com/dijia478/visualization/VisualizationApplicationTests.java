e5c497c395d1b5f1ea4175ff25e6c8683a851d5e
liuly <liuly@xiaoshouyi.com>
2023-08-05 14:54:23 +0800

    初始化项目

---
+++ src/test/java/com/dijia478/visualization/VisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dijia478.visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
